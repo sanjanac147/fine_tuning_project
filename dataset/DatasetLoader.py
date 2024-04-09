be25d5986d800bcee50b6d0eaf4900411517e436
ariyaltharun <ariyaltharun@gmail.com>
2024-04-09 18:24:34 +0000

    Update DatasetLoader.py and DatasetFactory file.py
    To load text dataset

--- dataset/DatasetLoader.py
+++ dataset/DatasetLoader.py
@@ -2,11 +2,11 @@ from abc import ABC, abstractmethod
 from datasets import load_dataset
 from DatasetLoader import DatasetLoader
 
+
 class IDatasetLoader(ABC):
     @abstractmethod
     def get_dataset(self):
         pass
-
 
 
 class ImageDatasetLoader(DatasetLoader):
@@ -31,8 +31,9 @@ class ImageDatasetLoader(DatasetLoader):
             print(f"Failed to load dataset: {e}")
             return None
 
+
 class TextDatasetLoader(DatasetLoader):
-    def __init__(self, dataset_name="food101", split="train[:5000]"):
+    def __init__(self, dataset_name: str, split: str) -> None:
         """
         Initializes the DatasetLoader with the specified dataset name and split configuration.
         :param dataset_name: The name of the dataset to load.
