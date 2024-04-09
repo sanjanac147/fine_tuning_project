22407b38a845da5cd2f7c5ff97c9c8252d130a69
sanjanac147 <126351480+sanjanac147@users.noreply.github.com>
2024-04-09 17:54:50 +0530

    Update TrainFactory.py

---
+++ train/TrainFactory.py
@@ -0,0 +1,6 @@
+from trainLoader import TrainLoader
+
+class TrainFactory:
+    @staticmethod
+    def get_trainer(model, args, train_dataset, eval_dataset, tokenizer, compute_metrics, data_collator):
+        return TrainLoader(model, args, train_dataset, eval_dataset, tokenizer, compute_metrics, data_collator)
