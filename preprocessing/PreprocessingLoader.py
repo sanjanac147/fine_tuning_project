e38aef592986fc95beff3662d198e590eacca20b
sanjanac147 <126351480+sanjanac147@users.noreply.github.com>
2024-04-09 17:27:46 +0530

    Update PreprocessingLoader.py

---
+++ preprocessing/PreprocessingLoader.py
@@ -0,0 +1,8 @@
+from PreprocessingFactor import ImagePreprocessor
+
+class PreprocessingLoader:
+    def __init__(self, model_checkpoint):
+        self.preprocessor = ImagePreprocessor(model_checkpoint)
+
+    def get_preprocessor(self):
+        return self.preprocessor
