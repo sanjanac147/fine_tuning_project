538561405471e1510e0ab82c065375e3c7c28144
sanjanac147 <126351480+sanjanac147@users.noreply.github.com>
2024-04-11 16:22:37 +0530

    added genral params to  HyperParameterSet.py

--- hyper_parameters/HyperParameterSet.py
+++ hyper_parameters/HyperParameterSet.py
@@ -15,6 +15,27 @@ class LoRAHyperParameterSet:
             return default_params
         return {**default_params, **params}
         
+     def get_general_parameters(params=None):
+       
+        default_params = {
+            "remove_unused_columns": False,
+            "evaluation_strategy": "epoch",
+            "save_strategy": "epoch",
+            "learning_rate": 5e-3,
+            "per_device_train_batch_size": batch_size,
+            "gradient_accumulation_steps": 4,
+            "per_device_eval_batch_size": batch_size,
+            "fp16": True,
+            "num_train_epochs": 10,
+            "logging_steps": 10,
+            "load_best_model_at_end": True,
+            "metric_for_best_model": "accuracy",
+            "label_names": ["labels"],
+        }
+        
+        if params is None:
+            return default_params
+        return {**default_params, **params}   
 class QLoRAHyperParameterSet:
     pass
 
